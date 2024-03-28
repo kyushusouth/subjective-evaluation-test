23ccb6e5278cfa749a59e545389e2924fc4293ff
Taisuke Minami <tmnm13009@gmail.com>
2024-03-28 22:08:03 +0900

    GCSのドメイン名を取得するためのAPIを作成

---
+++ app/api/domainName/route.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: "../../.env" });
+
+export function GET(request: Request) {
+    const domainName = process.env.GCS_DOMAIN_NAME
+    return Response.json(domainName);
+}
