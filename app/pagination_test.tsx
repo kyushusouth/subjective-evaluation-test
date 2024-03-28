49ffcacf4f0c6d408e022584e9715dd6e89ee6aa
Taisuke Minami <tmnm13009@gmail.com>
2024-03-28 22:19:41 +0900

    エラーするのでコメントアウト

--- app/pagination_test.tsx
+++ app/pagination_test.tsx
@@ -1,32 +1,32 @@
-'use client';
+// 'use client';
 
-import { fetchInvoicesPages } from "@/app/lib/data";
-import Pagination from "@/components/pagination";
-import Eval from "@/components/eval"
-import { fetchFilteredWavFiles } from "@/app/lib/data";
-import React, { useState } from 'react';
+// import { fetchInvoicesPages } from "@/app/lib/data";
+// import Pagination from "@/components/pagination";
+// import Eval from "@/components/eval"
+// import { fetchFilteredWavFiles } from "@/app/lib/data";
+// import React, { useState } from 'react';
 
-export default async function Index({
-  searchParams
-}: {
-  searchParams?: {
-    page?: string;
-  }
-}) {
-  const [data, setData] = useState();
-  const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchInvoicesPages();
-  const sampleMetaDataList = await fetchFilteredWavFiles(currentPage);
+// export default async function Index({
+//   searchParams
+// }: {
+//   searchParams?: {
+//     page?: string;
+//   }
+// }) {
+//   const [data, setData] = useState();
+//   const currentPage = Number(searchParams?.page) || 1;
+//   const totalPages = await fetchInvoicesPages();
+//   const sampleMetaDataList = await fetchFilteredWavFiles(currentPage);
 
-  return (
-    <div>
-      <h1>Root Page</h1>
+//   return (
+//     <div>
+//       <h1>Root Page</h1>
 
-      <Eval currentPage={currentPage} data={data} setData={setData}></Eval>
+//       <Eval currentPage={currentPage} data={data} setData={setData}></Eval>
 
-      <div className="my-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
-      </div>
-    </div>
-  )
-}
+//       <div className="my-5 flex w-full justify-center">
+//         <Pagination totalPages={totalPages} />
+//       </div>
+//     </div>
+//   )
+// }
