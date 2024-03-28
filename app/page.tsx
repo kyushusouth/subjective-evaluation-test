2ec64268f1835f957820b1fc82b207cfee54a510
Taisuke Minami <tmnm13009@gmail.com>
2024-03-28 22:07:10 +0900

    Indexページの変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,7 @@
-import { PrismaClient } from '@prisma/client'
-import dotenv from 'dotenv';
-
-dotenv.config({ path: "../.env" });
-const domainName = process.env.GCS_DOMAIN_NAME
-const bucketName = process.env.GCS_BUCKET_NAME
-const prisma = new PrismaClient()
-
-export default async function Index() {
-  const users = await prisma.respondents.findMany()
-  const sampleMetaDataList = await prisma.sampleMetaData.findMany()
-  const urlList = []
-  for (let sampleMetaData of sampleMetaDataList) {
-    urlList.push(domainName + "/" + bucketName + "/" + sampleMetaData.file_path)
-  }
-
+export default function Index() {
   return (
     <div>
-      {urlList.map((url, index) => (
-        <div key={index}>
-          <audio src={url} controls controlsList="nodownload"></audio>
-        </div>
-      ))}
+      Index
     </div>
   )
 }
