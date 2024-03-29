061e79fb320cdbe64cfb5845c811ea85c0c132d4
Taisuke Minami <tmnm13009@gmail.com>
2024-03-29 17:12:36 +0900

    LoginButtonコンポーネントなしの実装に変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import LoginButton from "@/components/LoginButton";
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Index() {
+  const { data: session, status } = useSession()
+
   return (
     <div>
-      <LoginButton></LoginButton>
+      {
+        session ? (
+          <div>
+            Signed in as {session.user.name} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        ) : (
+          <div>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+          </div>
+        )
+      }
     </div>
   )
 }
