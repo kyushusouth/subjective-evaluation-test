bf0c5e5769cb08d1cb31a645c52fe1c68504df3a
Taisuke Minami <tmnm13009@gmail.com>
2024-03-29 16:50:03 +0900

    NextAuth設定の追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@
 import { inter } from "@/app/fonts"
 import Link from 'next/link';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { NextAuthProvider } from "@/app/NextAuthProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -13,27 +16,34 @@ export const metadata = {
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <AppRouterCacheProvider>
-          <main className="min-h-screen flex flex-col items-center">
-            <ul>
-              <li>
-                <Link href="/">root</Link>
-              </li>
-              <li>
-                <Link href="/eval">eval</Link>
-              </li>
-            </ul>
-            {children}
-          </main>
-        </AppRouterCacheProvider>
+        <NextAuthProvider session={session}>
+          <AppRouterCacheProvider>
+            <main className="min-h-screen flex flex-col items-center">
+              <ul>
+                <li>
+                  <Link href="/">root</Link>
+                </li>
+                <li>
+                  <Link href="/eval">eval</Link>
+                </li>
+                <li>
+                  <Link href="/login">login</Link>
+                </li>
+              </ul>
+              {children}
+            </main>
+          </AppRouterCacheProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
