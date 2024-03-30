9a791a73dee6cd333652d1aa93f5a5e072ccfc87
Taisuke Minami <tmnm13009@gmail.com>
2024-03-30 10:31:05 +0900

    ビルド時エラーのためコメントアウト

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,24 +1,24 @@
-"use client";
+// "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react"
-import { useRouter } from "next/navigation";
+// import { useSession, signIn, signOut } from "next-auth/react"
+// import { useRouter } from "next/navigation";
 
-export default function LoginButton() {
-    const { data: session, status } = useSession()
-    const router = useRouter();
+// export default function LoginButton() {
+//     const { data: session, status } = useSession()
+//     const router = useRouter();
 
-    if (session) {
-        return (
-            <div>
-                Signed in as {session.user.name} <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </div>
-        )
-    }
-    return (
-        <div>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
-        </div>
-    )
-}
+//     if (session) {
+//         return (
+//             <div>
+//                 Signed in as {session.user.name} <br />
+//                 <button onClick={() => signOut()}>Sign out</button>
+//             </div>
+//         )
+//     }
+//     return (
+//         <div>
+//             Not signed in <br />
+//             <button onClick={() => signIn()}>Sign in</button>
+//         </div>
+//     )
+// }
