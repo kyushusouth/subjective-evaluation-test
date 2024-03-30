62a21d180b8113f86a594c04fa0ff4b24193871f
Taisuke Minami <tmnm13009@gmail.com>
2024-03-30 10:15:05 +0900

    nextauthの設定削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@
 import { inter } from "@/app/fonts"
 import Link from 'next/link';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import { NextAuthProvider } from "@/app/NextAuthProvider";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -21,29 +18,25 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <NextAuthProvider session={session}>
-          <AppRouterCacheProvider>
-            <main className="min-h-screen flex flex-col items-center">
-              <ul>
-                <li>
-                  <Link href="/">root</Link>
-                </li>
-                <li>
-                  <Link href="/eval">eval</Link>
-                </li>
-                <li>
-                  <Link href="/login">login</Link>
-                </li>
-              </ul>
-              {children}
-            </main>
-          </AppRouterCacheProvider>
-        </NextAuthProvider>
+        <AppRouterCacheProvider>
+          <main className="min-h-screen flex flex-col items-center">
+            <ul>
+              <li>
+                <Link href="/">root</Link>
+              </li>
+              <li>
+                <Link href="/eval">eval</Link>
+              </li>
+              <li>
+                <Link href="/login">login</Link>
+              </li>
+            </ul>
+            {children}
+          </main>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react"
-
 export default function Index() {
-  const { data: session, status } = useSession()
-
   return (
     <div>
-      {
-        session ? (
-          <div>
-            Signed in as {session.user.name} <br />
-            <button onClick={() => signOut()}>Sign out</button>
-          </div>
-        ) : (
-          <div>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
-          </div>
-        )
-      }
+      hello
     </div>
   )
 }
