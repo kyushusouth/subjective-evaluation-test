e4d6e1b35f0b2edb0605ac96b4516a2c25bbf294
Taisuke Minami <tmnm13009@gmail.com>
2024-04-01 19:52:20 +0900

    Drawerコンポーネント実装

---
+++ app/ToggleDrawer.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+export default function ToggleDrawer() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List
+        sx={{
+          mt: 5,
+        }}
+      >
+        {[
+          ["ホーム", "/"],
+          ["年齢・性別", "/info"],
+          ["練習試行", "/eval"],
+        ].map((item, index) => (
+          <ListItem key={item[0]} disablePadding>
+            <ListItemButton component="a" href={item[1]}>
+              <ListItemText primary={item[0]} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)} variant="contained">
+        Open drawer
+      </Button>
+      <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
