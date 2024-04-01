c29fdf184c535cf308fc3068cc5672a94a287e35
Taisuke Minami <tmnm13009@gmail.com>
2024-04-01 18:44:59 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,77 @@
-"use client";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import SubmitButton from "@/app/login/submit-button";
+import Container from "@mui/material/Container";
 
-export default function Index() {
+export default async function Index() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  const signOut = async () => {
+    "use server";
+
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/");
+  };
+
   return (
     <div>
-      hello
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+        <div>ようこそ {user.id}</div>
+
+        <form action={signOut}>
+          <SubmitButton>ログアウト</SubmitButton>
+        </form>
+      </Container>
     </div>
-  )
+  );
 }
