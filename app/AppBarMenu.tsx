8669b878197e83e12739471d5b8a4cc9e0389e26
Taisuke Minami <tmnm13009@gmail.com>
2024-04-01 19:52:01 +0900

    Drawerボタンの実装

--- app/AppBarMenu.tsx
+++ app/AppBarMenu.tsx
@@ -2,9 +2,8 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+import ToggleDrawer from "@/app//ToggleDrawer";
 
 export default function AppBarMenu({ isLoggedIn }: { isLoggedIn: boolean }) {
   return (
@@ -14,17 +13,7 @@ export default function AppBarMenu({ isLoggedIn }: { isLoggedIn: boolean }) {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             主観評価実験
           </Typography>
-          {isLoggedIn ? (
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-          ) : null}
+          {isLoggedIn ? <ToggleDrawer /> : null}
         </Toolbar>
       </Container>
     </AppBar>
