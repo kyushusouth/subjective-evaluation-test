6fee5e42cd85c7feb4648f7747ffbd00a1a97a0f
Taisuke Minami <tmnm13009@gmail.com>
2024-04-02 20:34:02 +0900

    評価フォームの最終提出ページ実装

--- app/eval/confirm.tsx
+++ app/eval/confirm.tsx
@@ -1,18 +1,29 @@
+"use client";
+
+/* eslint-disable no-restricted-syntax */
 import { useFormContext } from "react-hook-form";
 import { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 import { SchemaType } from "@/app/eval/schema";
+import { SampleMetaData, Respondents } from "@prisma/client";
+import { useRouter } from "next/navigation";
 
-export default function Confirm({ onPrev }: { onPrev: () => void }) {
+export default function Confirm({
+  onPrev,
+  sampleMetaDataList,
+  respondent,
+}: {
+  onPrev: () => void;
+  sampleMetaDataList: SampleMetaData[];
+  respondent: Respondents;
+}) {
+  const router = useRouter();
   const methods = useFormContext<SchemaType>();
-
   const { handleSubmit } = methods;
-
   const [sendData, setSendData] = useState<SchemaType | null>(null);
 
   function onSubmit(data: SchemaType) {
-    console.log("success", data);
     setSendData(data);
   }
 
@@ -24,20 +35,58 @@ export default function Confirm({ onPrev }: { onPrev: () => void }) {
     handleSubmit(onSubmit, onError)();
   }, []);
 
+  const handleClick = () => {
+    const fetchDataList = async (dataList: object[]) => {
+      const response = await fetch("api/answers", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataList),
+        method: "POST",
+      });
+      const result = await response.json();
+      if (result.success) {
+        router.push("/");
+      } else {
+        router.push("/eval");
+      }
+    };
+
+    if (sendData === null) {
+      console.error("sendData is null.");
+      return;
+    }
+    const dataList = [];
+    for (const sampleMetaData of sampleMetaDataList) {
+      const sampleId = Number(sampleMetaData.id);
+      const naturalness = Number(sendData[`naturalness_${sampleId}`]);
+      const intelligibility = Number(sendData[`intelligibility_${sampleId}`]);
+      dataList.push({
+        respondent_id: respondent.id,
+        sample_meta_data_id: sampleId,
+        naturalness_id: naturalness,
+        intelligibility_id: intelligibility,
+      });
+    }
+    fetchDataList(dataList);
+  };
+
   return (
-    <div>
-      <Box display="flex" justifyContent="center" gap={2} mt={2}>
-        <Button variant="contained" onClick={onPrev}>
-          戻る
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => console.log(JSON.stringify(sendData))}
-          disabled={!sendData}
-        >
-          送信
-        </Button>
-      </Box>
-    </div>
+    <Container
+      sx={{
+        my: 10,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 4,
+      }}
+    >
+      <Button variant="contained" onClick={onPrev}>
+        戻る
+      </Button>
+      <Button variant="contained" onClick={handleClick} disabled={!sendData}>
+        送信
+      </Button>
+    </Container>
   );
 }
