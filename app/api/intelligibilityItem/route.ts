fe71c460a37b4516a5af08637b01b7706b1b6cff
Taisuke Minami <tmnm13009@gmail.com>
2024-04-02 20:33:00 +0900

    intelligibilityItemを取得するためのapiを実装

---
+++ app/api/intelligibilityItem/route.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+import { prisma } from "@/app/lib/prisma";
+
+export async function GET(request: Request) {
+  try {
+    const intelligibilityItem = await prisma.intelligibilityItem.findMany();
+    return Response.json(intelligibilityItem);
+  } catch (error) {
+    throw new Error("Failed to fetch intelligibility item.");
+  }
+}
