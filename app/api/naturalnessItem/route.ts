a47f7aef82d7c65163f7be35d39822f23c5d8005
Taisuke Minami <tmnm13009@gmail.com>
2024-04-02 20:33:16 +0900

    naturalnessItemを取得するためのapi実装

---
+++ app/api/naturalnessItem/route.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+import { prisma } from "@/app/lib/prisma";
+
+export async function GET(request: Request) {
+  try {
+    const naturalnessItem = await prisma.naturalnessItem.findMany();
+    return Response.json(naturalnessItem);
+  } catch (error) {
+    throw new Error("Failed to fetch naturalness item.");
+  }
+}
