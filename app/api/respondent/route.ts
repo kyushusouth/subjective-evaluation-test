8e49c43df6f1e3826895228cd68f41160bcb7138
Taisuke Minami <tmnm13009@gmail.com>
2024-04-02 20:33:29 +0900

    respondentを取得するためのapi実装

---
+++ app/api/respondent/route.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/prefer-default-export */
+import { prisma } from "@/app/lib/prisma";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export async function GET(request: Request) {
+  const supabase = createClient();
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user === null) {
+      redirect("/login");
+    }
+    const respondent = await prisma.respondents.findUnique({
+      where: {
+        auth_id: user.id,
+      },
+    });
+    return Response.json(respondent);
+  } catch (error) {
+    throw new Error("Failed to fetch naturalness item.");
+  }
+}
