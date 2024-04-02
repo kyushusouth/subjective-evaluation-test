d9efee5ca99250a748c18a5228bb7a95d5de0e17
Taisuke Minami <tmnm13009@gmail.com>
2024-04-02 20:37:16 +0900

    seeding用プログラムの修正

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,7 +11,6 @@ const storage = new Storage();
 
 function getWavFilesInDirectory(directoryPath: string): string[] {
   const files: string[] = [];
-  const savePath: string[] = [];
   const items = fs.readdirSync(directoryPath);
   for (const item of items) {
     const itemPath = path.join(directoryPath, item);
@@ -59,33 +58,20 @@ async function deleteFile(bucketName: string, fileName: string) {
 async function main() {
   const localWavDir = process.env.LOCAL_WAV_DIR;
   if (localWavDir === undefined) {
-    console.error("LOCAL_WAV_DIR was not specified.");
-    return;
+    throw new Error("LOCAL_WAV_DIR was not specified.");
   }
   const bucketName = process.env.GCS_BUCKET_NAME;
   if (bucketName === undefined) {
-    console.error("GCS_BUCKET_NAME was not specified.");
-    return;
+    throw new Error("GCS_BUCKET_NAME was not specified.");
   }
-
-  // const numRespondents = 30;
-  // for (let i = 0; i < numRespondents; i++) {
-  //     await prisma.respondents.create({
-  //         data: {
-  //             name: uuidv4(),
-  //             password: generateRandomString(8),
-  //             sex: "",
-  //             age: -1,
-  //         },
-  //     })
-  // }
 
   const gcsFiles = await listFiles(bucketName);
   for (let file of gcsFiles) {
-    await deleteFile(bucketName, file.name);
+    deleteFile(bucketName, file.name);
   }
 
   const filePathList = getWavFilesInDirectory(localWavDir);
+  const sampleMetaDataList = [];
   for (let filePath of filePathList) {
     const filePathParts = filePath.split("/");
     const speakerName = filePathParts[filePathParts.length - 3];
@@ -94,46 +80,48 @@ async function main() {
     const kind = filePathParts[filePathParts.length - 1].split(".")[0];
     const randomizedFilePath = uuidv4() + ".wav";
     uploadFile(bucketName, filePath, randomizedFilePath);
-    await prisma.sampleMetaData.create({
-      data: {
-        file_path: randomizedFilePath,
-        speaker_name: speakerName,
-        model_name: modelName,
-        sample_name: sampleName,
-        kind: kind,
-      },
+    sampleMetaDataList.push({
+      file_path: randomizedFilePath,
+      speaker_name: speakerName,
+      model_name: modelName,
+      sample_name: sampleName,
+      kind: kind,
     });
   }
+  await prisma.sampleMetaData.createMany({
+    data: sampleMetaDataList,
+    skipDuplicates: true,
+  });
+
+  const sexItemList = [{ item: "男性" }, { item: "女性" }, { item: "無回答" }];
+  await prisma.sexItem.createMany({
+    data: sexItemList,
+    skipDuplicates: true,
+  });
 
   const naturalnessItemList = [
-    "非常に悪い",
-    "悪い",
-    "普通",
-    "良い",
-    "非常に良い",
+    { item: "非常に悪い" },
+    { item: "悪い" },
+    { item: "普通" },
+    { item: "良い" },
+    { item: "非常に良い" },
   ];
-  for (let naturalnessItem of naturalnessItemList) {
-    await prisma.naturalnessItem.create({
-      data: {
-        item: naturalnessItem,
-      },
-    });
-  }
+  await prisma.naturalnessItem.createMany({
+    data: naturalnessItemList,
+    skipDuplicates: true,
+  });
 
   const intelligibilityItemList = [
-    "非常に悪い",
-    "悪い",
-    "普通",
-    "良い",
-    "非常に良い",
+    { item: "非常に悪い" },
+    { item: "悪い" },
+    { item: "普通" },
+    { item: "良い" },
+    { item: "非常に良い" },
   ];
-  for (let intelligibilityItem of intelligibilityItemList) {
-    await prisma.intelligibilityItem.create({
-      data: {
-        item: intelligibilityItem,
-      },
-    });
-  }
+  await prisma.intelligibilityItem.createMany({
+    data: intelligibilityItemList,
+    skipDuplicates: true,
+  });
 }
 
 main()
