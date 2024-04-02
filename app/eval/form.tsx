5eb7d3b1c8adc90ec058b2124c820849fe908181
Taisuke Minami <tmnm13009@gmail.com>
2024-04-02 20:34:26 +0900

    評価フォーム用コンポーネントの実装

--- app/eval/form.tsx
+++ app/eval/form.tsx
@@ -5,7 +5,16 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import { SampleMetaData } from "@prisma/client";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import {
+  SampleMetaData,
+  NaturalnessItem,
+  IntelligibilityItem,
+} from "@prisma/client";
 import { SchemaType } from "@/app/eval/schema";
 
 export default function Form({
@@ -14,6 +23,8 @@ export default function Form({
   sampleMetaDataList,
   domainName,
   bucketName,
+  naturalnessItemList,
+  intelligibilityItemList,
   pageNumber,
 }: {
   onNext: () => void;
@@ -21,6 +32,8 @@ export default function Form({
   sampleMetaDataList: SampleMetaData[];
   domainName: string;
   bucketName: string;
+  naturalnessItemList: NaturalnessItem[];
+  intelligibilityItemList: IntelligibilityItem[];
   pageNumber: number;
 }) {
   const methods = useFormContext<SchemaType>();
@@ -32,55 +45,97 @@ export default function Form({
   } = methods;
 
   return (
-    <div>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        my: 10,
+      }}
+    >
       {sampleMetaDataList.map((data, index) => {
         const sampleId = data.id;
-        const sampleUrl = `${domainName  }/${  bucketName  }/${  data.file_path}`;
+        const sampleUrl = `${domainName}/${bucketName}/${data.file_path}`;
         return (
-          <div key={sampleId}>
-            <div>
-              {sampleId}: {sampleUrl}
-            </div>
-            <audio src={sampleUrl} controls controlsList="nodownload" />
-            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-              <InputLabel id={`${sampleId}_naturalness_label`}>
-                自然性
-              </InputLabel>
-              <Select
-                labelId={`${sampleId}_naturalness_label`}
-                label={`${sampleId}_naturalness`}
-                {...register(`${sampleId}_naturalness`)}
-                defaultValue={getValues(`${sampleId}_naturalness`)}
-              >
-                <MenuItem value={1}>1: 非常に悪い</MenuItem>
-                <MenuItem value={2}>2: 悪い</MenuItem>
-                <MenuItem value={3}>3: 普通</MenuItem>
-                <MenuItem value={4}>4: 良い</MenuItem>
-                <MenuItem value={5}>5: 非常に良い</MenuItem>
-              </Select>
-            </FormControl>
-
-            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-              <InputLabel id={`${sampleId}_intelligibility_label`}>
-                明瞭性
-              </InputLabel>
-              <Select
-                labelId={`${sampleId}_intelligibility_label`}
-                label={`${sampleId}_intelligibility`}
-                {...register(`${sampleId}_intelligibility`)}
-                defaultValue={getValues(`${sampleId}_intelligibility`)}
-              >
-                <MenuItem value={1}>1: 非常に悪い</MenuItem>
-                <MenuItem value={2}>2: 悪い</MenuItem>
-                <MenuItem value={3}>3: 普通</MenuItem>
-                <MenuItem value={4}>4: 良い</MenuItem>
-                <MenuItem value={5}>5: 非常に良い</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
+          <Card key={sampleId} sx={{ mb: 3, p: 2, width: 0.8, maxWidth: 500 }}>
+            <CardContent sx={{ mb: 1, ml: 1, p: 0 }}>
+              <Typography component="div">No. {sampleId}</Typography>
+            </CardContent>
+            <CardMedia
+              src={sampleUrl}
+              component="audio"
+              controls
+              controlsList="nodownload"
+              sx={{ mb: 2, p: 0 }}
+            />
+            <Box
+              sx={{
+                p: 0,
+                m: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                alignContent: "center",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                gap: 2,
+              }}
+            >
+              <FormControl sx={{ p: 0, m: 0, minWidth: 120 }} size="small">
+                <InputLabel id={`naturalness_${sampleId}_label`}>
+                  自然性
+                </InputLabel>
+                <Select
+                  labelId={`naturalness_${sampleId}_label`}
+                  label={`naturalness_${sampleId}`}
+                  {...register(`naturalness_${sampleId}`)}
+                  defaultValue={getValues(`naturalness_${sampleId}`)}
+                >
+                  {naturalnessItemList.map((naturalnessItem, index) => (
+                    <MenuItem
+                      key={naturalnessItem.id}
+                      value={naturalnessItem.id}
+                    >
+                      {naturalnessItem.id}: {naturalnessItem.item}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl sx={{ p: 0, m: 0, minWidth: 120 }} size="small">
+                <InputLabel id={`intelligibility_${sampleId}_label`}>
+                  明瞭性
+                </InputLabel>
+                <Select
+                  labelId={`intelligibility_${sampleId}_label`}
+                  label={`intelligibility_${sampleId}`}
+                  {...register(`intelligibility_${sampleId}`)}
+                  defaultValue={getValues(`intelligibility_${sampleId}`)}
+                >
+                  {intelligibilityItemList.map((intelligibilityItem, index) => (
+                    <MenuItem
+                      key={intelligibilityItem.id}
+                      value={intelligibilityItem.id}
+                    >
+                      {intelligibilityItem.id}: {intelligibilityItem.item}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </Card>
         );
       })}
-      <Box display="flex" justifyContent="center" gap={2} mt={2}>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 4,
+          mt: 2,
+        }}
+      >
         <Button
           variant="contained"
           onClick={onPrev}
@@ -92,6 +147,6 @@ export default function Form({
           次へ
         </Button>
       </Box>
-    </div>
+    </Container>
   );
 }
